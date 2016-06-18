f462ca4bc7c725bc61644bc7d72cc9073de362c8
mizegit <dzm1991@gmail.com>
2016-06-18 15:57:45 +0800

    entab and detab

---
+++ 01-21-zm.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#define TABINC 4
+#define MAXLINE 1000
+
+int getline(char s[],int maxline);
+void entab(char s[],int tabinc);
+
+char s[MAXLINE];
+char st[MAXLINE];
+
+main()
+{
+	int i;
+
+	while ( (i = getline(s,MAXLINE) ) > 0)
+	{
+        entab(s,TABINC);
+        printf("%s", s);
+	}
+}
+
+void entab(char s[], int tabinc){
+    int i, j, k, l, pos;
+
+    j = 0;
+    pos = 0;
+    for (i=0; s[i]!='\0' ; ++i){
+        if (s[i] == '\t'){
+            s[j] = '\t';
+            ++j;
+            pos = pos + (tabinc - pos % tabinc);
+        }else if ( (pos + 1) % tabinc == 0 && s[i] == ' '){
+            printf("pos - %d\n", pos);
+            printf("got a blank before tab stop\n");
+            k = i;
+            l = tabinc;
+            while (s[k] == ' ' && l > 0){
+                printf("s[i] - %c\n", s[i]);
+                --l;
+                --j;
+                --k;
+            }
+            ++j;
+            s[j] = '\t';
+            ++j;
+            ++pos;
+        }else{
+            s[j] = s[i];
+            ++j;
+            ++pos;
+        }
+    }
+    s[j] = '\0';
+}
+
+/* getline: read a line into s, return length */
+int getline(char s[], int lim)
+{
+    int c, i, j;
+
+    j = 0;
+    for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i)
+        if (i < lim -2){        /* line still in boundaries */
+            s[j] = c;
+            ++j;
+        }
+    if (c == '\n'){
+        s[j] = c;
+        ++j;
+        ++i;
+    }
+    s[j] = '\0';
+    return i;
+}
