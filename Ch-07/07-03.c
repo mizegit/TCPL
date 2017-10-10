c6f7a47fed5505c901aeb4dcc671ddd89ec4396a
Mize <dzm1991@gmail.com>
2017-10-10 14:01:29 +0800

    07-03 miniprintf

--- Ch-07/07-03.c
+++ Ch-07/07-03.c
@@ -2,43 +2,62 @@
 #include <stdarg.h>
 #include <ctype.h>
 
+#define LOCALFMT 100
+
 void miniprintf(char *fmt, ...);
 
 main()
 {
-	miniprintf("digit:%d float:%f", 111, 111.1);
+	miniprintf("digit:%d unsigned:%u", 111, -1111);
 }
 
+/* minprintf: minimal printf with variable argument list */
 void miniprintf(char *fmt, ...)
 {
-	va_list ap;
-	char *p, *sval;
-	int ival;
-	double dval;
-	
-	va_start(ap, fmt);
-	for (p = fmt; *p; p++) {
-		if (*p != '%') {
-			putchar(*p);
-			continue;
-		}
-		switch (*++p) {
-		case 'd':
-			ival = va_arg(ap, int);
-			printf("%d", ival);
-			break;
-		case 'f':
-			dval = va_arg(ap, double);
-			printf("%f", dval);
-			break;
-		case 's':
-			for (sval = va_arg(ap, char *); *sval; sval++)
-				putchar(*sval);
-			break;
-		default:
-			putchar(*p);
-			break;
-		}
-	}
-	va_end(ap);
+    va_list ap;
+    char *p, *sval;
+    char localfmt[LOCALFMT];
+    int i, ival;
+    unsigned uval;
+    double dval;
+
+    va_start(ap, fmt); /* make ap point to 1st unnamed arg*/
+    for (p = fmt; *p; p++) {
+        if (*p != '%') {
+            putchar(*p);
+            continue;
+        }
+        i = 0;
+        localfmt[i++] = '%';
+        while (*(p+1) && !isalpha(*(p+1)))
+            localfmt[i++] = *++p;
+        localfmt[i++] = *(p+1);
+        localfmt[i] = '\0';
+        switch(*++p) {
+        case 'd':
+        case 'i':
+            ival = va_arg(ap, int);
+            printf(localfmt, ival);
+            break;
+        case 'x':
+        case 'X':
+        case 'u':
+        case 'o':
+            uval = va_arg(ap, unsigned);
+            printf(localfmt, uval);
+            break;
+        case 'f':
+            dval = va_arg(ap, double);
+            printf(localfmt, dval);
+            break;
+        case 's':
+            sval = va_arg(ap, char *);
+            printf(localfmt, sval);
+            break;
+        default:
+            printf(localfmt);
+            break;
+        }
+    }
+    va_end(ap);
 }
