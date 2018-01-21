217451a974d7ec2cc18e3f564b979d83ad8044c3
Mize <dzm1991@gmail.com>
2018-01-21 11:22:49 +0800

    ch8.6

--- Ch-08/08-04.c
+++ Ch-08/08-04.c
@@ -10,7 +10,7 @@ int fseek(FILE *fp, long offset, int origin)
         rc = lseek(fp->flag & _WRITE);
         fp->cnt = 0;
     } else if (fp->flag & _WRITE) {
-        if ((nc = fp->ptr - fp->base , nc) != nc)
+        if ((nc = fp->ptr - fp->base, nc) != nc)
             rc = 1;
         if (rc != -1)
             rc = lseek(fp->fd, offset, origin);
